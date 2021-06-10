2420b2fc61bcc7bb43402e2d9580e061b1fbe84e
fernando <760156389@qq.com>
2021-06-10 17:08:12 +0800

    t

--- CDD-master/app/src/main/java/com/saveandstudio/mario/cdd/Components/HandCardManager.java
+++ CDD-master/app/src/main/java/com/saveandstudio/mario/cdd/Components/HandCardManager.java
@@ -77,6 +77,8 @@ public class HandCardManager extends MonoBehavior {
 
 
         // test
+//        Decoder decoder = new Decoder(Global.encodedString);
+//        Decoder decoder = new Decoder("0,1,1,0,2,0,2,3,4,0,5,0,6,0,8,0,8,1,10,0,10,2,10,3,11,1,1,2,3,0,3,2,4,1,4,2,5,1,7,0,7,1,7,3,8,2,8,3,9,0,12,1,0,2,0,3,1,3,4,3,6,1,6,2,7,2,9,2,10,1,11,0,11,2,12,2,12,3,0,0,1,1,2,1,2,2,3,1,3,3,5,2,5,3,6,3,9,1,9,3,11,3,12,0");
         Decoder decoder = new Decoder(Global.encodedString);
         cardPackages = new ArrayList<>();
         cardPackages = decoder.decode();
